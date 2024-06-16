845587c775e085a3b2708bf4f2587751212ae3f8
yang <cryptopunkyang@pm.me>
2024-06-16 23:24:56 +0200

    fix(target/js): rollup + goja fails on portal import

--- target/js/embed/portal/portal.js
+++ target/js/embed/portal/portal.js
@@ -11,26 +11,34 @@ function inject(platform, adapter) {
 
 // ================== Wails bindings adapter ==================
 
+
+let wails = {};
+try {
+  wails = window;
+} catch (e) {
+
+}
+
 /* eslint-disable */
-const platform$3 = typeof window['go'] === "undefined" ? undefined : "wails";
+const platform$3 = typeof wails['go'] === "undefined" ? undefined : "wails";
 
 /* eslint-disable */
 const adapter$2 = () => ({
   // apphost
-  astral_conn_accept: window['go']['main']['Adapter']['ConnAccept'],
-  astral_conn_close: window['go']['main']['Adapter']['ConnClose'],
-  astral_conn_read: window['go']['main']['Adapter']['ConnRead'],
-  astral_conn_write: window['go']['main']['Adapter']['ConnWrite'],
-  astral_node_info: window['go']['main']['Adapter']['NodeInfo'],
-  astral_query: window['go']['main']['Adapter']['Query'],
-  astral_query_name: window['go']['main']['Adapter']['QueryName'],
-  astral_resolve: window['go']['main']['Adapter']['Resolve'],
-  astral_service_close: window['go']['main']['Adapter']['ServiceClose'],
-  astral_service_register: window['go']['main']['Adapter']['ServiceRegister'],
-  astral_interrupt: window['go']['main']['Adapter']['Interrupt'],
+  astral_conn_accept: wails['go']['main']['Adapter']['ConnAccept'],
+  astral_conn_close: wails['go']['main']['Adapter']['ConnClose'],
+  astral_conn_read: wails['go']['main']['Adapter']['ConnRead'],
+  astral_conn_write: wails['go']['main']['Adapter']['ConnWrite'],
+  astral_node_info: wails['go']['main']['Adapter']['NodeInfo'],
+  astral_query: wails['go']['main']['Adapter']['Query'],
+  astral_query_name: wails['go']['main']['Adapter']['QueryName'],
+  astral_resolve: wails['go']['main']['Adapter']['Resolve'],
+  astral_service_close: wails['go']['main']['Adapter']['ServiceClose'],
+  astral_service_register: wails['go']['main']['Adapter']['ServiceRegister'],
+  astral_interrupt: wails['go']['main']['Adapter']['Interrupt'],
   // runtime
-  sleep: window['go']['main']['Adapter']['Sleep'],
-  log: async (...arg) => await window['go']['main']['Adapter']['LogArr'](arg),
+  sleep: wails['go']['main']['Adapter']['Sleep'],
+  log: async (...arg) => await wails['go']['main']['Adapter']['LogArr'](arg),
 });
 
 inject(platform$3, adapter$2);
