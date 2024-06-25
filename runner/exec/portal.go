5dbb2320ab5fd6ccd0d09643f0170a1d9f88bc25
yang <cryptopunkyang@pm.me>
2024-06-25 21:05:08 +0200

    refactor

--- runner/exec/portal.go
+++ runner/exec/portal.go
@@ -2,9 +2,12 @@ package exec
 
 import (
 	"context"
+	"fmt"
 	"github.com/cryptopunkscc/go-astral-js/pkg/plog"
 	"github.com/cryptopunkscc/go-astral-js/target"
+	"io"
 	"os"
+	"os/exec"
 	"path"
 )
 
@@ -46,4 +49,61 @@ func (r *Runner[T]) Run(ctx context.Context, src T) (err error) {
 		return r.newRunBundleExecutable(ctx, v)
 	}
 	return
+}
+
+type portal[T target.Portal] struct {
+	src []string
+}
+
+func newPortal[T target.Portal](src ...string) *portal[T] {
+	return &portal[T]{src: src}
+}
+
+func (p *portal[T]) run(ctx context.Context, src T) (err error) {
+	cmd := p.src[0]
+	args := append(p.src[1:], src.Abs())
+	var c *exec.Cmd
+	if ctx != nil {
+		c = exec.CommandContext(ctx, cmd, args...)
+	} else {
+		c = exec.Command(cmd, args...)
+	}
+	c.Stdout = os.Stdout
+	c.Stderr = os.Stderr
+	c.Stdin = os.Stdin
+	err = c.Run()
+	if err != nil {
+		err = fmt.Errorf("exec.Portal '%s': %w", p.src, err)
+	}
+	return
+}
+
+func (r *Runner[T]) newRunBundleExecutable(ctx context.Context, v target.BundleExec) error {
+	p := v.Executable().Lift().Path()
+	temp, err := os.CreateTemp(r.cacheDir, p)
+	if err != nil {
+		return plog.Err(err)
+	}
+	e := v.Executable()
+	file, err := e.Files().Open(e.Path())
+	if err != nil {
+		return plog.Err(err)
+	}
+	if err = temp.Chmod(0755); err != nil {
+		return plog.Err(err)
+	}
+	_, err = io.Copy(temp, file)
+	if err != nil {
+		return plog.Err(err)
+	}
+	if err = temp.Close(); err != nil {
+		return plog.Err(err)
+	}
+	defer os.Remove(temp.Name())
+	_ = file.Close()
+	err = newPortal[target.Portal](temp.Name()).run(ctx, v)
+	if err != nil {
+		return err
+	}
+	return nil
 }
