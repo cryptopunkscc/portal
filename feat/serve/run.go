183fced1f10453a1c6157e21f010da59f683212b
yang <cryptopunkyang@pm.me>
2024-06-25 21:07:38 +0200

    feat(serve): start astral daemon if needed

--- feat/serve/run.go
+++ feat/serve/run.go
@@ -4,25 +4,42 @@ import (
 	"context"
 	"fmt"
 	"github.com/cryptopunkscc/astrald/auth/id"
+	"github.com/cryptopunkscc/go-astral-js/pkg/apphost"
 	"github.com/cryptopunkscc/go-astral-js/pkg/plog"
 	"github.com/cryptopunkscc/go-astral-js/pkg/rpc"
 	"github.com/cryptopunkscc/go-astral-js/target"
 )
 
+// Feat representing portal service.
 type Feat struct {
-	port  target.Port
-	tray  target.Tray
-	serve target.Dispatch
+	astral Astral
+	port   target.Port
+	tray   target.Tray
+	serve  target.Dispatch
 }
 
 type (
-	Observe   func(ctx context.Context, conn rpc.Conn) (err error)
-	Install   func(src string) error
+	// Astral starts daemon if not already running.
+	Astral func(ctx context.Context) (err error)
+
+	// Observe on installed applications.
+	Observe func(ctx context.Context, conn rpc.Conn) (err error)
+
+	// Install application from given source (fs path).
+	Install func(src string) error
+
+	// Uninstall application with given id.
 	Uninstall func(id string) error
-	Service   func(handlers rpc.Handlers) target.Dispatch
+
+	// Service starts portal rpc with additional handlers.
+	Service func(handlers rpc.Handlers) target.Dispatch
 )
 
+// CheckAstral is a default implementation of Astral function. Returns error if astral is not started.
+func CheckAstral(_ context.Context) error { return apphost.Check() }
+
 func NewFeat(
+	astral Astral,
 	port target.Port,
 	service Service,
 	handlers rpc.Handlers,
@@ -37,16 +54,22 @@ func NewFeat(
 	handlers["open"] = spawn
 	handlers["observe"] = observe
 	return &Feat{
-		port:  port,
-		tray:  tray,
-		serve: service(handlers),
+		astral: astral,
+		port:   port,
+		tray:   tray,
+		serve:  service(handlers),
 	}
 }
 
+// Run portal service including astral daemon if not started. Optionally displays an indicator in OS tray.
 func (f Feat) Run(
 	ctx context.Context,
 	tray bool,
 ) (err error) {
+	if err = f.astral(ctx); err != nil {
+		return
+	}
+
 	log := plog.Get(ctx).Type(f).Set(&ctx)
 	request := rpc.NewRequest(id.Anyone, f.port.String())
 	if err = rpc.Command(request, "ping"); err == nil {
---
+++ runner/exec/astrald.go
@@ -0,0 +1,33 @@
+package exec
+
+import (
+	"context"
+	"github.com/cryptopunkscc/go-astral-js/pkg/apphost"
+	exec2 "github.com/cryptopunkscc/go-astral-js/pkg/exec"
+	"os"
+	"os/exec"
+	"time"
+)
+
+// Astral starts astral daemon process in a given [context.Context] and waits until available.
+func Astral(ctx context.Context) (err error) {
+
+	// check if astrald already running
+	if err = apphost.Check(); err == nil {
+		return
+	}
+
+	// start astrald process
+	cmd := exec.CommandContext(ctx, "astrald")
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.Env = os.Environ()
+	if err = cmd.Start(); err != nil {
+		return
+	}
+
+	// await for apphost
+	return exec2.Retry(ctx, 10*time.Second, func(int, int, time.Duration) (err error) {
+		return apphost.Init()
+	})
+}
