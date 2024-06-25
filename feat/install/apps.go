6fa5a6973f3f1e3a05605bbb9b30d4e04a47e2f8
yang <cryptopunkyang@pm.me>
2024-06-25 22:35:18 +0200

    feat/install: fix portal js lib injection for embed apps

--- feat/install/apps.go
+++ feat/install/apps.go
@@ -5,14 +5,17 @@ import (
 	"github.com/cryptopunkscc/go-astral-js/feat/build"
 	"github.com/cryptopunkscc/go-astral-js/runner/dist"
 	"github.com/cryptopunkscc/go-astral-js/runner/pack"
+	"github.com/cryptopunkscc/go-astral-js/target"
+	"github.com/cryptopunkscc/go-astral-js/target/sources"
 	"log"
+	"path"
 )
 
 func (d *PortalDev) buildJsApps() {
-	if len(d.modules) == 0 {
-		d.collectPortalLibs()
-	}
-	feat := build.NewFeat(dist.NewRun, pack.Run, d.modules...)
+	dir := path.Join(d.root, "target/js/embed/portal")
+	libs := sources.FromPath[target.NodeModule](dir)
+
+	feat := build.NewFeat(dist.NewRun, pack.Run, libs...)
 	if err := feat.Dist(context.TODO(), d.root, "apps"); err != nil {
 		log.Fatal(err)
 	}
