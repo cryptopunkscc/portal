05d6db52b97e03cad3e77ee96c0596d1ca05e5e6
yang <cryptopunkyang@pm.me>
2024-07-22 18:14:02 +0200

    release v0.0.0

--- manifest.go
+++ manifest.go
@@ -1,12 +1,10 @@
 package portal
 
 const (
-	Version        = "v0.0.1-alpha"
+	Version        = "v0.0.0"
 	Name           = "Portal"
 	NameDev        = "Portal Development"
-	Description    = "Runtime environment for Astral apps."
 	DescriptionDev = "Development environment for Astral apps."
 	AstralVersion  = "v0.0.0-20240510192132-6172557ff205"
-	WailsVersion   = "v2.9.0"
 	Module         = "github.com/cryptopunkscc/portal/"
 )
