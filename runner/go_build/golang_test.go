c58d9a0f5ef341ba844ec056f60fe5a6e1b003ca
yang <cryptopunkyang@pm.me>
2024-08-14 19:50:42 +0200

    runner/go_build: fix test

--- runner/go_build/golang_test.go
+++ runner/go_build/golang_test.go
@@ -3,27 +3,20 @@ package go_build
 import (
 	"context"
 	golang "github.com/cryptopunkscc/portal/resolve/go"
-	"github.com/cryptopunkscc/portal/resolve/source"
-	"github.com/cryptopunkscc/portal/target"
+	"github.com/cryptopunkscc/portal/test"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
 func TestGoRunner_Run(t *testing.T) {
+	defer test.Clean()
+	src := test.Copy(test.EmbedGo)
 	ctx := context.Background()
-	file, err := source.File("test", "test")
+	project, err := golang.ResolveProject(src)
 	if err != nil {
 		t.Fatal(err)
 	}
-	project, err := golang.ResolveProject(file)
-	if err != nil {
-		t.Fatal(err)
-	}
-	expected := target.Builds{
-		"default": target.Build{Deps: []string(nil), Env: []string(nil), Cmd: "go build -o dist/main"},
-		"windows": target.Build{Deps: []string(nil), Env: []string(nil), Cmd: "go build -o dist/main.exe"},
-	}
-	assert.Equal(t, expected, project.Build())
+	assert.Equal(t, test.EmbedGoBuild, project.Build())
 	run := Runner()
 	if err = run(ctx, project); err != nil {
 		t.Fatal(err)
