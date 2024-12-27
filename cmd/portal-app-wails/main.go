5a7c9ec3854ad2ce666e8df494159e3820dce141
yang <cryptopunkyang@pm.me>
2024-12-27 01:24:14 +0100

    portal/cmd: migrate to rpc v2

--- cmd/portal-app-wails/main.go
+++ cmd/portal-app-wails/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"context"
 	. "github.com/cryptopunkscc/portal/api/target"
-	"github.com/cryptopunkscc/portal/clir"
 	"github.com/cryptopunkscc/portal/factory/app"
 	factory "github.com/cryptopunkscc/portal/factory/bind"
 	"github.com/cryptopunkscc/portal/feat/open"
@@ -12,21 +11,33 @@ import (
 	"github.com/cryptopunkscc/portal/pkg/sig"
 	"github.com/cryptopunkscc/portal/runner/wails"
 	"github.com/cryptopunkscc/portal/runtime/bind"
+	"github.com/cryptopunkscc/portal/runtime/rpc2/cli"
+	"github.com/cryptopunkscc/portal/runtime/rpc2/cmd"
 )
 
 func main() {
-	m := &Module{}
+	mod := &Module{}
 	ctx, cancel := context.WithCancel(context.Background())
 	log := plog.New().D().Scope("app-wails").Set(&ctx)
 	go sig.OnShutdown(cancel)
-	cli := clir.NewCli(ctx,
-		"Portal-wails",
-		"Portal html runner driven by wails.",
-		version.Run,
-	)
-	cli.Open(open.Feat[AppHtml](m))
 
-	if err := cli.Run(); err != nil {
+	err := cli.New(cmd.Handler{
+		Name: "portal-app-wails",
+		Desc: "Portal html runner driven by wails.",
+		Sub: cmd.Handlers{
+			{
+				Func: open.Feat[AppHtml](mod),
+				Name: "o",
+				Desc: "Start portal app in wails runner.",
+				Params: cmd.Params{
+					{Type: "string", Desc: "Absolute path to app bundle or directory."},
+				},
+			},
+			{Name: "v", Desc: "Print version", Func: version.Run},
+		},
+	}).Run(ctx)
+
+	if err != nil {
 		log.Println(err)
 	}
 	cancel()
