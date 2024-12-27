4424ad5f71b8b19a3121ea7d5b6f74f6c4987ed0
yang <cryptopunkyang@pm.me>
2024-12-27 01:24:14 +0100

    portal/cmd: migrate to rpc2

--- cmd/portal-app-goja/main.go
+++ cmd/portal-app-goja/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"context"
 	. "github.com/cryptopunkscc/portal/api/target"
-	"github.com/cryptopunkscc/portal/clir"
 	"github.com/cryptopunkscc/portal/factory/app"
 	"github.com/cryptopunkscc/portal/factory/bind"
 	"github.com/cryptopunkscc/portal/feat/open"
@@ -11,6 +10,8 @@ import (
 	"github.com/cryptopunkscc/portal/pkg/plog"
 	"github.com/cryptopunkscc/portal/pkg/sig"
 	"github.com/cryptopunkscc/portal/runner/goja"
+	"github.com/cryptopunkscc/portal/runtime/rpc2/cli"
+	"github.com/cryptopunkscc/portal/runtime/rpc2/cmd"
 )
 
 func main() {
@@ -18,13 +19,24 @@ func main() {
 	ctx, cancel := context.WithCancel(context.Background())
 	log := plog.New().D().Scope("app-goja").Set(&ctx)
 	go sig.OnShutdown(cancel)
-	cli := clir.NewCli(ctx,
-		"Portal-goja",
-		"Portal js runner driven by goja.",
-		version.Run,
-	)
-	cli.Open(open.Feat[AppJs](mod))
-	if err := cli.Run(); err != nil {
+
+	err := cli.New(cmd.Handler{
+		Name: "portal-app-goja",
+		Desc: "Portal js runner driven by goja.",
+		Sub: cmd.Handlers{
+			{
+				Func: open.Feat[AppJs](mod),
+				Name: "o",
+				Desc: "Start portal app in goja runner.",
+				Params: cmd.Params{
+					{Type: "string", Desc: "Absolute path to app bundle or directory."},
+				},
+			},
+			{Name: "v", Desc: "Print version", Func: version.Run},
+		},
+	}).Run(ctx)
+
+	if err != nil {
 		log.Println(err)
 	}
 	cancel()
