bb091709b3cea7812035ff8fac2f052973fd1587
yang <cryptopunkyang@pm.me>
2025-01-05 16:59:00 +0100

    cmd/portal: use cli runner

--- cmd/portal/main.go
+++ cmd/portal/main.go
@@ -1,25 +1,18 @@
 package main
 
 import (
-	"context"
 	"github.com/cryptopunkscc/portal/api/apphost"
 	"github.com/cryptopunkscc/portal/api/target"
 	"github.com/cryptopunkscc/portal/feat/start"
 	"github.com/cryptopunkscc/portal/feat/version"
-	"github.com/cryptopunkscc/portal/pkg/plog"
-	"github.com/cryptopunkscc/portal/pkg/sig"
 	"github.com/cryptopunkscc/portal/request/exec"
 	"github.com/cryptopunkscc/portal/request/query"
-	"github.com/cryptopunkscc/portal/runtime/rpc2/cli"
+	"github.com/cryptopunkscc/portal/runner/cli"
 	"github.com/cryptopunkscc/portal/runtime/rpc2/cmd"
 )
 
 func main() {
-	ctx, cancel := context.WithCancel(context.Background())
-	log := plog.New().D().Scope("app").Set(&ctx)
-	go sig.OnShutdown(cancel)
-
-	err := cli.New(cmd.Handler{
+	cli.Run(cmd.Handler{
 		Name: "portal",
 		Desc: "Portal command line.",
 		Func: start.Feat(deps{}),
@@ -29,12 +22,7 @@ func main() {
 		Sub: cmd.Handlers{
 			{Name: "v", Desc: "Print version", Func: version.Run},
 		},
-	}).Run(ctx)
-
-	if err != nil {
-		log.Println(err)
-	}
-	cancel()
+	})
 }
 
 type deps struct{}
