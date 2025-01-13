d2e960155c4208479d3ec52897ac66e0518db758
yang <cryptopunkyang@pm.me>
2025-01-13 03:46:57 +0100

    apps/go-dev: migrate from portal-dev-go

---
+++ apps/go-dev/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	. "github.com/cryptopunkscc/portal/api/target"
+	"github.com/cryptopunkscc/portal/factory/bind"
+	"github.com/cryptopunkscc/portal/factory/dev"
+	"github.com/cryptopunkscc/portal/resolve/sources"
+	"github.com/cryptopunkscc/portal/runner/cli"
+	"github.com/cryptopunkscc/portal/runner/exec"
+	"github.com/cryptopunkscc/portal/runner/go_dev"
+	"github.com/cryptopunkscc/portal/runner/multi"
+	"github.com/cryptopunkscc/portal/runner/open"
+	"github.com/cryptopunkscc/portal/runner/reload"
+	"github.com/cryptopunkscc/portal/runner/version"
+	"github.com/cryptopunkscc/portal/runtime/rpc2/cmd"
+)
+
+func main() { cli.Run(Application[ProjectGo]{}.handler()) }
+
+type Application[T ProjectGo] struct{ dev.Module[T] }
+
+func (a Application[T]) handler() cmd.Handler {
+	return cmd.Handler{
+		Func: open.Runner[T](a),
+		Name: "dev-go",
+		Desc: "Start portal golang app development.",
+		Params: cmd.Params{
+			{Type: "string", Desc: "Absolute path to app bundle or directory."},
+		},
+		Sub: cmd.Handlers{
+			{Name: "v", Desc: "Print version", Func: version.Run},
+		},
+	}
+}
+
+func (a Application[T]) Runner() Run[T] {
+	return multi.Runner[T](
+		reload.Mutable(bind.DefaultRuntime(), PortMsg, go_dev.Adapter(exec.Dist().Run)),
+	)
+}
+
+func (a Application[T]) Resolver() Resolve[T] { return sources.Resolver[T]() }
