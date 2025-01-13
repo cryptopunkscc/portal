44aa4abb3050435b8d832eb158cb80922fff6839
yang <cryptopunkyang@pm.me>
2025-01-13 03:46:35 +0100

    apps/exec-dev: migrate from portal-dev-exec

---
+++ apps/exec-dev/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	. "github.com/cryptopunkscc/portal/api/target"
+	"github.com/cryptopunkscc/portal/factory/bind"
+	"github.com/cryptopunkscc/portal/resolve/sources"
+	"github.com/cryptopunkscc/portal/runner/cli"
+	"github.com/cryptopunkscc/portal/runner/exec"
+	"github.com/cryptopunkscc/portal/runner/multi"
+	"github.com/cryptopunkscc/portal/runner/open"
+	"github.com/cryptopunkscc/portal/runner/reload"
+	"github.com/cryptopunkscc/portal/runner/version"
+	"github.com/cryptopunkscc/portal/runtime/rpc2/cmd"
+)
+
+func main() { cli.Run(Application[AppExec]{}.handler()) }
+
+type Application[T AppExec] struct{}
+
+func (a Application[T]) handler() cmd.Handler {
+	return cmd.Handler{
+		Func: open.Runner[T](&a),
+		Name: "dev-exec",
+		Desc: "Portal development runner for executables.",
+		Params: cmd.Params{
+			{Type: "string", Desc: "Absolute path to app bundle or directory."},
+		},
+		Sub: cmd.Handlers{
+			{Name: "v", Desc: "Print version", Func: version.Run},
+		},
+	}
+}
+
+func (a Application[T]) Runner() Run[T] {
+	return multi.Runner[T](
+		reload.Immutable(bind.DefaultRuntime(), PortMsg, reload.Adapter(exec.Bundle(CacheDir("portal-dev")))),
+		reload.Immutable(bind.DefaultRuntime(), PortMsg, reload.Adapter(exec.Dist())),
+	)
+}
+
+func (a Application[T]) Resolver() Resolve[T] { return sources.Resolver[T]() }
