651a37e81774988a6bb8f0799a723929ad960a6e
yang <cryptopunkyang@pm.me>
2025-01-23 19:27:57 +0100

    remove obsolete implementation

--- runner/exec/portal.go
+++
@@ -1,9 +0,0 @@
-package exec
-
-import (
-	"github.com/cryptopunkscc/portal/api/target"
-)
-
-func Portal[T target.Portal_](command ...string) target.Runner[T] {
-	return Runner[T](func(T) ([]string, error) { return command, nil })
-}
