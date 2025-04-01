6b4ce0270d433c54e38a6b815287620c2b081148
yang <cryptopunkyang@pm.me>
2025-04-01 08:22:53 +0200

    env/desktop: set apps and tokens defaults

--- api/env/desktop/init.go
+++ api/env/desktop/init.go
@@ -9,7 +9,11 @@ func init() {
 	env.PortaldHome.Default(defaultPortalHome)
 	env.AstraldHome.Default(defaultAstraldHome)
 	env.AstraldDb.Default(defaultAstraldHome)
+	env.PortaldTokens.Default(defaultTokensDir)
+	env.PortaldApps.Default(defaultTokensDir)
 }
 
 func defaultPortalHome() string  { return filepath.Join(userConfigDir(), "portald") }
 func defaultAstraldHome() string { return filepath.Join(userConfigDir(), "astrald") }
+func defaultTokensDir() string   { return filepath.Join(env.PortaldTokens.Get(), "tokens") }
+func defaultAppsDir() string     { return filepath.Join(env.PortaldTokens.Get(), "apps") }
