d6ecd026d882ddba3f189f208db6a4b5ab7689a0
yang <cryptopunkyang@pm.me>
2025-08-03 20:12:08 +0200

    apps/launcher: fix API client

--- apps/launcher/svelte/src/portal.js
+++ apps/launcher/svelte/src/portal.js
@@ -2,10 +2,10 @@ import {log, rpc} from "portal";
 
 log("launcher start")
 
-const client = rpc.target("portald").bind("open", "install", "uninstall")
+const client = rpc.target("portald").bind("open", {"app": ["install", "uninstall"]})
 
 client.observe = async () => {
-  const conn = await rpc.target("portald").conn("list.observe")
+  const conn = await rpc.target("portald").conn("app.list.observe")
   return {
     next: async () => await conn.decode(),
     more: async (num) => await conn.encode(num),
