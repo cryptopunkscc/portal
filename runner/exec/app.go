1f068364fc3714308778384e64dc5a88406f4890
yang <cryptopunkyang@pm.me>
2025-12-30 16:29:25 +0100

    runner/exec: refactor AppRunner.RunApp

--- runner/exec/app.go
+++ runner/exec/app.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"os"
 	"os/exec"
+	"slices"
 
 	"github.com/cryptopunkscc/astrald/lib/apphost"
 	"github.com/cryptopunkscc/portal/api/manifest"
@@ -20,37 +21,37 @@ type AppRunner struct{ portal.Config }
 var _ RunApp = AppRunner{}.RunApp
 
 func (d AppRunner) RunApp(ctx context.Context, manifest manifest.App, path string, args ...string) (err error) {
+	defer plog.TraceErr(&err)
 	tokens := token.Repository{Dir: d.Config.Tokens}
 	t, err := tokens.Get(manifest.Package)
 	if err != nil {
 		return err
 	}
-	return d.run(ctx, t.Token.String(), path, args...)
-}
+	authToken := t.Token.String()
 
-func WithReadWriter(ctx context.Context, rw io.ReadWriter) context.Context {
-	return context.WithValue(ctx, stdKey, &Std{In: rw, Out: rw, Err: rw})
-}
-
-func (d AppRunner) run(ctx context.Context, token string, path string, args ...string) (err error) {
-	defer plog.TraceErr(&err)
 	log := plog.Get(ctx).Type(d).Set(&ctx)
-	log.Printf("Command run: %s, %v", path, args)
+	log.Printf("app run: %s, %v", path, args)
+
 	cmd := exec.CommandContext(ctx, path, args...)
 	cmd.Cancel = func() error { return cmd.Process.Signal(os.Interrupt) }
-	cmd.Env = append(os.Environ(), apphost.AuthTokenEnv+"="+token)
-	cmd.Env = append(cmd.Env, d.Env()...)
+	cmd.Env = slices.Concat(os.Environ(), d.Env(), []string{apphost.AuthTokenEnv + "=" + authToken})
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	cmd.Stdin = os.Stdin
+
 	if err = tryRedirectStd(ctx, cmd); err != nil {
 		return
 	}
 	if err = cmd.Run(); err != nil {
 		err = plog.Err(err)
 	}
-	log.Printf("Command finished: %s, %v", path, args)
+
+	log.Printf("app finished: %s, %v", path, args)
 	return
+}
+
+func WithReadWriter(ctx context.Context, rw io.ReadWriter) context.Context {
+	return context.WithValue(ctx, stdKey, &Std{In: rw, Out: rw, Err: rw})
 }
 
 func tryRedirectStd(ctx context.Context, cmd *exec.Cmd) (err error) {
